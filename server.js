027665de2e851b36b15f5349b0648984b9dda06e
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-26 11:40:33 +0530

    add dot env config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 require("./config/dbConnect");
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));

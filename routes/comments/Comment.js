f11fc6198650f82a5dd6b1371ac0a6bfc2e38bd2
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-26 17:23:58 +0530

    commment route updated

--- routes/comments/Comment.js
+++ routes/comments/Comment.js
@@ -1,66 +1,20 @@
 const express = require("express");
+const {
+  comCreatedCtrl,
+  getComCtrl,
+  delComCtrl,
+  upComCtrl,
+} = require("../../controllers/comments/comment");
 const commentRoutes = express.Router();
 
 //!POST/api/v1/comments
-commentRoutes.post("/", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "comment created",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
+commentRoutes.post("/", comCreatedCtrl);
 
 //!GET/api/v1/comments/:id
-commentRoutes.get("/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "Comments details",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
+commentRoutes.get("/:id", getComCtrl);
 //!DEl/api/v1/comments/:id
-commentRoutes.delete("/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "Comment delete",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
+commentRoutes.delete("/:id", delComCtrl);
 //!PUT/api/v1/comments/:id
-commentRoutes.put("/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "Comment updated",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
+commentRoutes.put("/:id", upComCtrl);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports=commentRoutes
+module.exports = commentRoutes;

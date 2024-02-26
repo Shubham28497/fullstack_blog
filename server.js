9a601d18bf33446e9d5c180d84a9fc8fd7f56c42
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-26 16:44:55 +0530

    updated server file

--- server.js
+++ server.js
@@ -2,66 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const userRoutes = require("./routes/users/User");
 const postRoutes = require("./routes/posts/Post");
+const commentRoutes = require("./routes/comments/Comment");
 const app = express();
 
 require("./config/dbConnect");
 //*middleware
 
-
 //* user route
 app.use("/api/v1/users", userRoutes);
 
 //*post route
-app.use("/api/v1/posts",postRoutes)
-
-
-
+app.use("/api/v1/posts", postRoutes);
 
 //*comment route
-//!POST/api/v1/comments
-app.post("/api/v1/comments", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "comment created",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-//!GET/api/v1/comments/:id
-app.get("/api/v1/comments/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "Comments details",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
-//!DEl/api/v1/comments/:id
-app.delete("/api/v1/comments/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "Comment delete",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
-//!PUT/api/v1/comments/:id
-app.put("/api/v1/comments/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      user: "Comment updated",
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
+app.use("/api/v1/comments", commentRoutes);
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));

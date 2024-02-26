9f512605e82678303cf35d24a9c2f614af694845
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-26 11:40:07 +0530

    add db connection string

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongoUrl");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("DB connected succesfully");
   } catch (err) {
     console.log("Db connection failed", err.message);

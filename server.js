125305332e7b5c712ff9b6080511d7b14d00af9d
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-26 11:31:00 +0530

    add db connect in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
-
+require("./config/dbConnect");
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));

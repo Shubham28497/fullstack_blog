f1db03da620dd48c420e8e89e3682058b011b9fe
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-27 11:30:45 +0530

    add error handler middleware

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const userRoutes = require("./routes/users/User");
 const postRoutes = require("./routes/posts/Post");
 const commentRoutes = require("./routes/comments/Comment");
+const globalErrHandler = require("./middlewares/globalHandler");
 const app = express();
 
 require("./config/dbConnect");
 //*middleware
-app.use(express.json())
+app.use(express.json());
 //* user route
 app.use("/api/v1/users", userRoutes);
 
@@ -16,5 +17,8 @@ app.use("/api/v1/posts", postRoutes);
 
 //*comment route
 app.use("/api/v1/comments", commentRoutes);
+
+//*error handler middleware
+app.use(globalErrHandler)
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));

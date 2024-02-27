2b444a64aade39daa6970165325ab0769f9125c1
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-27 12:15:12 +0530

    add session configuration

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const userRoutes = require("./routes/users/User");
 const postRoutes = require("./routes/posts/Post");
 const commentRoutes = require("./routes/comments/Comment");
 const globalErrHandler = require("./middlewares/globalHandler");
+const session = require("express-session");
 const app = express();
 
 require("./config/dbConnect");
 //*middleware
 app.use(express.json());
+
+//*session config
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 //* user route
 app.use("/api/v1/users", userRoutes);
 
@@ -19,6 +30,6 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/comments", commentRoutes);
 
 //*error handler middleware
-app.use(globalErrHandler)
+app.use(globalErrHandler);
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));

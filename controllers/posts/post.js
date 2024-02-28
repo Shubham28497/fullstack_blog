49acb87973bffedf3dc775f86261ce5ea50b1df1
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-28 10:32:50 +0530

    add to create a post logic

--- controllers/posts/post.js
+++ controllers/posts/post.js
@@ -1,9 +1,29 @@
+const Post = require("../../model/post/Post");
+const User = require("../../model/user/User");
+
 //* post created
 const postCreatedCtrl = async (req, res) => {
+  const { title, description, category, image, user } = req.body;
   try {
+    //* find the user
+    const userID = req.session.userAuth;
+    // console.log(userID);
+    const userFound = await User.findById(userID);
+    //* create post
+    const postCreated = await Post.create({
+      title,
+      description,
+      category,
+      user: userFound._id,
+    });
+
+    //* push the post created into the arrayof user's post
+    userFound.posts.push(postCreated._id);
+    //* re save
+    await userFound.save();
     res.json({
       status: "Success",
-      user: "Post created",
+      data: postCreated,
     });
   } catch (err) {
     res.json(err);

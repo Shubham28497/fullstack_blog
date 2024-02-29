01d1d5ddcdce2ede2fab377bda3c279ff40bd475
Shubham28497 <shubhamgupta@cdac.in>
2024-02-29 11:53:01 +0530

    updated controller

--- controllers/posts/post.js
+++ controllers/posts/post.js
@@ -37,7 +37,7 @@ const postCreatedCtrl = async (req, res, next) => {
 //*get all posts
 const getPostCtrl = async (req, res, next) => {
   try {
-    const getPosts = await Post.find();
+    const getPosts = await Post.find().populate('comments');
     res.json({
       status: "Success",
       data: getPosts,

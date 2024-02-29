1a4e26797228aaf8f7f0950eefa6c04d6d10a32e
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-29 11:06:50 +0530

    add create comment logic

--- controllers/comments/comment.js
+++ controllers/comments/comment.js
@@ -1,9 +1,30 @@
+const Post = require("../../model/post/Post");
+const Comment = require("../../model/comment/Comment");
+const User = require("../../model/user/User");
 //* comment created
 const comCreatedCtrl = async (req, res) => {
+  const { message } = req.body;
   try {
+    //* find the post
+    const post = await Post.findById(req.params.id);
+    //* create the comment
+    const comment = await Comment.create({
+      user: req.session.userAuth,
+      message,
+    });
+    //* push the comment to post
+    post.comments.push(comment._id);
+    //* find the user
+    const user = await User.findById(req.session.userAuth);
+    //* push the comment into
+    user.comments.push(comment._id);
+    //* disable validation
+    //* save
+    await post.save({ validateBeforeSave :false});
+    await user.save({validateBeforeSave:false})
     res.json({
       status: "Success",
-      user: "comment created",
+      data: comment,
     });
   } catch (err) {
     res.json(err);

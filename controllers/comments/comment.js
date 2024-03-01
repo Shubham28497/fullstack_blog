6322a80d965eceaecd905410042328640fc0a4ab
Shubham28497 <shubhamgupta@cdac.in>
2024-03-01 09:48:33 +0530

    add logic to update the comment

--- controllers/comments/comment.js
+++ controllers/comments/comment.js
@@ -1,8 +1,9 @@
 const Post = require("../../model/post/Post");
 const Comment = require("../../model/comment/Comment");
 const User = require("../../model/user/User");
+const appErr = require("../../utils/appErr");
 //* comment created
-const comCreatedCtrl = async (req, res) => {
+const comCreatedCtrl = async (req, res,next) => {
   const { message } = req.body;
   try {
     //* find the post
@@ -20,30 +21,30 @@ const comCreatedCtrl = async (req, res) => {
     user.comments.push(comment._id);
     //* disable validation
     //* save
-    await post.save({ validateBeforeSave :false});
-    await user.save({validateBeforeSave:false})
+    await post.save({ validateBeforeSave: false });
+    await user.save({ validateBeforeSave: false });
     res.json({
       status: "Success",
       data: comment,
     });
   } catch (err) {
-    res.json(err);
+   next(appErr(err));
   }
 };
 //*get comment
-const getComCtrl = async (req, res) => {
+const getComCtrl = async (req, res,next) => {
   try {
     res.json({
       status: "Success",
       user: "Comments details",
     });
   } catch (err) {
-    res.json(err);
+   next(appErr(err));
   }
 };
 
 //*del comment
-const delComCtrl = async (req, res) => {
+const delComCtrl = async (req, res,next) => {
   try {
     //* find a post
     const comment = await Comment.findById(req.params.id);
@@ -58,18 +59,36 @@ const delComCtrl = async (req, res) => {
       data: "Comment has been deleted succesfully",
     });
   } catch (err) {
-    res.json(err);
+    next(appErr(err));
   }
 };
 //*update comment
-const upComCtrl = async (req, res) => {
+const upComCtrl = async (req, res, next) => {
   try {
+    //* find a post
+    const comment = await Comment.findById(req.params.id);
+    if(!comment){
+      return next(appErr("Comment not found"))
+    }
+    //*check if the post belong to the user
+    if (comment.user.toString() !== req.session.userAuth.toString()) {
+      return next(appErr("You are not allowed to update this comment", 403));
+    }
+    const commentUpdated = await Comment.findByIdAndUpdate(
+      req.params.id,
+      {
+        message: req.body.message,
+      },
+      {
+        new: true,
+      }
+    );
     res.json({
       status: "Success",
-      user: "Comment updated",
+      data: commentUpdated,
     });
   } catch (err) {
-    res.json(err);
+    next(appErr(err));
   }
 };
 module.exports = {
